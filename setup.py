767ff72738df685670e73df7d398dd478f13bf4b
Birjot Bala <birjot.bala@gmail.com>
2020-07-23 17:42:04 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open('requirements.txt', "r") as f:
+    requirements = f.read().splitlines()
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name='osrs_discord_bot',
+    version="0.1.dev",
+    author="Birjot Bala",
+    description="Discord Bot for accessing OSRS APIs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Birjot-Bala/OSRS-Discord-Bot",
+    packages=["osrs_discord_bot"],
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ],
+    python_requires='>3.6',
+)
