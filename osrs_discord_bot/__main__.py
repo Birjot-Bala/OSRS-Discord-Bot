3b8bf40fc7c9fe4af5f733abfb8af9f56c8391b2
Birjot Bala <birjot.bala@gmail.com>
2020-07-31 13:51:33 -0400

    Created bot.py

--- osrs_discord_bot/__main__.py
+++ osrs_discord_bot/__main__.py
@@ -2,62 +2,20 @@
 
 import locale
 
-from discord.ext import commands
-
-import osrs_discord_bot.services as se
 from osrs_discord_bot.settings import TOKEN
+from osrs_discord_bot.bot import bot
 
 
 locale.setlocale(locale.LC_ALL, '')
 
 
-# selected prefix for discord commands
-bot = commands.Bot(command_prefix='!')
-        
-
-@bot.event
-async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
+def run_bot():
+    bot.run(TOKEN)
 
 
-# !hiscore {skill} {username}
-@bot.command(name='hiscore', help='Posts player hiscores.')
-async def hiscore(ctx, skill, *args):
-    hiscore_message = se.hiscore_message(skill, *args)
-    await ctx.send(hiscore_message)
+def main():
+    run_bot()
 
 
-# !ge X posts GE price information of first 10 items that include X
-@bot.command(name='ge', help='Posts GE price of items')
-async def ge(ctx, *args):
-    ge_message = se.ge_message(*args)
-    await ctx.send(ge_message)
-     
-        
-# !wiki X posts a link to the wiki for X
-@bot.command(name='wiki', help='Pulls up wiki link')
-async def wiki(ctx, *args):
-    wiki_message = se.wiki_message(*args)
-    await ctx.send(wiki_message)
-
-
-@bot.command(
-    name='chance',
-    help='Calculates the percent chance of getting a drop within a set number of actions.'
-)
-#!chance X, Y calculates the chance of hitting the X drop rate in Y actions
-async def chance(ctx, droprate, actions=None):
-    chance_message = se.chance_message(droprate, actions)
-    await ctx.send(chance_message)
-
-
-@bot.command(
-    name='tracker',
-    help='Uses the Wise Old Man API to retrieve tracked XP gains.'
-)
-async def tracker(ctx, period, *args):
-    tracker_message = se.tracker_message(period, *args)
-    await ctx.send(tracker_message)
-
-    
-bot.run(TOKEN)
+if __name__ == '__main__':
+    main()
---
+++ osrs_discord_bot/bot.py
@@ -0,0 +1,53 @@
+# bot.py
+
+from discord.ext import commands
+
+import osrs_discord_bot.services as se
+
+
+bot = commands.Bot(command_prefix='!')
+        
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+
+# !hiscore {skill} {username}
+@bot.command(name='hiscore', help='Posts player hiscores.')
+async def hiscore(ctx, skill, *args):
+    hiscore_message = se.hiscore_message(skill, *args)
+    await ctx.send(hiscore_message)
+
+
+# !ge X posts GE price information of first 10 items that include X
+@bot.command(name='ge', help='Posts GE price of items')
+async def ge(ctx, *args):
+    ge_message = se.ge_message(*args)
+    await ctx.send(ge_message)
+     
+        
+# !wiki X posts a link to the wiki for X
+@bot.command(name='wiki', help='Pulls up wiki link')
+async def wiki(ctx, *args):
+    wiki_message = se.wiki_message(*args)
+    await ctx.send(wiki_message)
+
+
+@bot.command(
+    name='chance',
+    help='Calculates the percent chance of getting a drop within a set number of actions.'
+)
+#!chance X, Y calculates the chance of hitting the X drop rate in Y actions
+async def chance(ctx, droprate, actions=None):
+    chance_message = se.chance_message(droprate, actions)
+    await ctx.send(chance_message)
+
+
+@bot.command(
+    name='tracker',
+    help='Uses the Wise Old Man API to retrieve tracked XP gains.'
+)
+async def tracker(ctx, period, *args):
+    tracker_message = se.tracker_message(period, *args)
+    await ctx.send(tracker_message)
