e1ac793dc9d914415e2f2b89804bebcb23cfd79e
Birjot Bala <birjot.bala@gmail.com>
2021-01-28 22:20:34 -0500

    Typo fix

--- osrs_discord_bot/constants.py
+++ osrs_discord_bot/constants.py
@@ -9,6 +9,6 @@ SKILL_NAMES = [
       'overall', 'attack', 'defence', 'strength', 'hitpoints',
       'ranged', 'prayer', 'magic', 'cooking', 'woodcutting', 'fletching',
       'fishing', 'firemaking', 'crafting', 'smithing', 'mining', 
-      'herblore', 'agility', 'thieving', 'slayer', 'farming', 'runecraft',
+      'herblore', 'agility', 'thieving', 'slayer', 'farming', 'runecrafting',
       'hunter', 'construction'
 ]
